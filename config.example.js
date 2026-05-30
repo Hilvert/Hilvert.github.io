11b7ec7ca44a8ef409658e62b9592703eaafc2fb
XiaoQiang Du <dxq5252000@gmail.com>
2026-05-30 22:21:56 +0800

    Delete config.example.js

--- config.example.js
+++
@@ -1,6 +0,0 @@
-﻿window.APP_CONFIG = {
-  supabaseUrl: "https://YOUR-PROJECT-ID.supabase.co",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
-  bucketName: "zip-files",
-  packagesTable: "packages"
-};
